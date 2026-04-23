db37f8baba06c499174ca55153cda471d08ad649
MrChuse <aleksha007@mail.ru>
2026-04-23 10:59:45 +0300

    Added basic socket interaction

--- client.py
+++ client.py
@@ -1,6 +1,21 @@
 import pygame
 import pygame.draw
 
+
+# Echo client program
+import socket
+
+HOST = 'localhost'    # The remote host
+PORT = 50007          # The same port as used by the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    msg = input()
+    while msg:
+        s.sendall(msg.encode('utf-8'))
+        data = s.recv(1024)
+        print('Received', repr(data))
+        msg = input()
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
--- server.py
+++ server.py
@@ -1,6 +1,24 @@
 import pygame
 import pygame.draw
 
+# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            print('Received', addr, data)
+            if not data: break
+            conn.sendall(data)
+
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
